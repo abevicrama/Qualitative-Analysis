0f21daf3d452cc69c994866e11cda9fc45ae214f
abevicrama <abevicramacoc1@gmail.com>
2023-03-29 00:32:40 +0530

    quize sample
    added quize and mark system

--- group Project 01/main.c
+++ group Project 01/main.c
@@ -2,8 +2,18 @@
 #include <stdlib.h>
 #include <time.h>
 
+
+int marks = 0;
+
 int main()
 {
+    start();
+    main2();
+
+    return 0;
+}
+
+void start(){
     printf("               *******************************************************\n");
     printf("               *******************************************************\n");
     printf("               **                                                   **\n");
@@ -14,9 +24,6 @@ int main()
     printf("               **                                                   **\n");
     printf("               *******************************************************\n");
     printf("               *******************************************************\n");
-    main2();
-
-    return 0;
 }
 
 void main2(){
@@ -39,6 +46,7 @@ void main2(){
         }
         else if (l_or_q == 2){
             //if user choose quize
+            quize();
 
             break;
         }
@@ -387,8 +395,7 @@ void strloop(char str[],int len_str){
 
 }
 //time delay for animation
-void delay(int number_of_seconds)
-{
+void delay(int number_of_seconds){
     //
     int milli_seconds = 4* number_of_seconds;
 
@@ -399,3 +406,240 @@ void delay(int number_of_seconds)
     while (clock() < start_time + milli_seconds)
         ;
 }
+
+
+
+
+
+void quize(){
+
+    printf("   1 - Learn anions\n");
+    printf("   2 - Learn cations\n");
+    int qiz;
+    printf("   Please Enter Number : ");
+    scanf("%d",&qiz);
+
+    while ((qiz != 1)||(qiz != 2)){
+
+        if (qiz == 1){
+            anion_quize();
+            printf("Your marks : %d",marks);
+            break;
+        }
+        else if (qiz == 2){
+            //cation_quize();
+            printf("Your marks : %d",marks);
+            break;
+        }
+        else{
+            printf(   "   Please Enter valid Number : ");
+            scanf("%d",&qiz);
+        }
+
+    }
+}
+
+
+void anion_quize(){
+    AQ1();
+    if_correct_answer_1();
+    AQ2();
+    if_correct_answer_3();
+    AQ3();
+    if_correct_answer_2();
+    AQ4();
+    if_correct_answer_4();
+    AQ5();
+    if_correct_answer_1();
+    AQ6();
+    if_correct_answer_3();
+    AQ7();
+    if_correct_answer_3();
+    AQ8();
+    if_correct_answer_1();
+    AQ9();
+    if_correct_answer_4();
+    AQ10();
+    if_correct_answer_2();
+    AQ11();
+    if_correct_answer_1();
+
+
+
+}
+
+
+
+void AQ1(){
+    printf("q 1 1111111111111111111111111111111111111111111111111111111\n\n");
+    printf("    1 - first answer\n");
+    printf("    2 - first answer\n");
+    printf("    3 - first answer\n");
+    printf("    4 - first answer\n\n");
+}
+void AQ2(){
+    printf("q 22222222222222222222222222222222222222222222222222222222222\n\n");
+    printf("    1 - first answer\n");
+    printf("    2 - first answer\n");
+    printf("    3 - first answer\n");
+    printf("    4 - first answer\n\n");
+}
+void AQ3(){
+    printf("q 333333333333333333333333333333333333333333333333333333333333333\n\n");
+    printf("    1 - first answer\n");
+    printf("    2 - first answer\n");
+    printf("    3 - first answer\n");
+    printf("    4 - first answer\n\n");
+}
+void AQ4(){
+    printf("q 1 1111111111111111111111111111111111111111111111111111111\n\n");
+    printf("    1 - first answer\n");
+    printf("    2 - first answer\n");
+    printf("    3 - first answer\n");
+    printf("    4 - first answer\n\n");
+}
+void AQ5(){
+    printf("q 1 1111111111111111111111111111111111111111111111111111111\n\n");
+    printf("    1 - first answer\n");
+    printf("    2 - first answer\n");
+    printf("    3 - first answer\n");
+    printf("    4 - first answer\n\n");
+}
+void AQ6(){
+    printf("q 1 1111111111111111111111111111111111111111111111111111111\n\n");
+    printf("    1 - first answer\n");
+    printf("    2 - first answer\n");
+    printf("    3 - first answer\n");
+    printf("    4 - first answer\n\n");
+}
+void AQ7(){
+    printf("q 1 1111111111111111111111111111111111111111111111111111111\n\n");
+    printf("    1 - first answer\n");
+    printf("    2 - first answer\n");
+    printf("    3 - first answer\n");
+    printf("    4 - first answer\n\n");
+}
+void AQ8(){
+    printf("q 1 1111111111111111111111111111111111111111111111111111111\n\n");
+    printf("    1 - first answer\n");
+    printf("    2 - first answer\n");
+    printf("    3 - first answer\n");
+    printf("    4 - first answer\n\n");
+}
+void AQ9(){
+    printf("q 1 1111111111111111111111111111111111111111111111111111111\n\n");
+    printf("    1 - first answer\n");
+    printf("    2 - first answer\n");
+    printf("    3 - first answer\n");
+    printf("    4 - first answer\n\n");
+}
+void AQ10(){
+    printf("q 1 1111111111111111111111111111111111111111111111111111111\n\n");
+    printf("    1 - first answer\n");
+    printf("    2 - first answer\n");
+    printf("    3 - first answer\n");
+    printf("    4 - first answer\n\n");
+}
+void AQ11(){
+    printf("q 1 1111111111111111111111111111111111111111111111111111111\n\n");
+    printf("    1 - first answer\n");
+    printf("    2 - first answer\n");
+    printf("    3 - first answer\n");
+    printf("    4 - first answer\n\n");
+}
+
+
+int if_correct_answer_4(){
+
+    printf("Enter answer number\n");
+    int qiz;
+    scanf("%d",&qiz);
+    while ((qiz != 1)||(qiz != 2)||(qiz != 3)||(qiz != 4)){
+
+        if (qiz == 4){
+            printf("Congratulation.\n You answer is correct\n");
+            marks = marks + 1;
+            break;
+        }
+        else if ((qiz == 2)||(qiz == 1)||(qiz == 3)){
+            printf("Correct answer is 4\n");
+            break;
+        }
+        else{
+            printf(   "   Please Enter valid Number : ");
+            scanf("%d",&qiz);
+        }
+        return marks;
+}}
+
+int if_correct_answer_3(){
+
+    printf("Enter answer number\n");
+    int qiz;
+    scanf("%d",&qiz);
+    while ((qiz != 1)||(qiz != 2)||(qiz != 3)||(qiz != 4)){
+
+        if (qiz == 3){
+            printf("Congratulation.\n You answer is correct\n");
+            marks = marks + 1;
+            break;
+        }
+        else if ((qiz == 2)||(qiz == 1)||(qiz == 4)){
+            printf("Correct answer is 3\n");
+            break;
+        }
+        else{
+            printf(   "   Please Enter valid Number : ");
+            scanf("%d",&qiz);
+        }
+        return marks;
+}}
+
+
+int if_correct_answer_2(){
+
+    printf("Enter answer number\n");
+    int qiz;
+    scanf("%d",&qiz);
+    while ((qiz != 1)||(qiz != 2)||(qiz != 3)||(qiz != 4)){
+
+        if (qiz == 2){
+            printf("Congratulation.\n You answer is correct\n");
+            marks = marks + 1;
+            break;
+        }
+        else if ((qiz == 1)||(qiz == 3)||(qiz == 4)){
+            printf("Correct answer is 2\n");
+            break;
+        }
+        else{
+            printf(   "   Please Enter valid Number : ");
+            scanf("%d",&qiz);
+        }
+        return marks;
+}}
+
+
+
+int if_correct_answer_1(){
+
+    printf("Enter answer number\n");
+    int qiz;
+    scanf("%d",&qiz);
+    while ((qiz != 1)||(qiz != 2)||(qiz != 3)||(qiz != 4)){
+
+        if (qiz == 1){
+            printf("Congratulation.\n You answer is correct\n");
+            marks = marks + 1;
+            break;
+        }
+        else if ((qiz == 2)||(qiz == 3)||(qiz == 4)){
+            printf("Correct answer is 1 \n");
+            break;
+        }
+        else{
+            printf(   "   Please Enter valid Number : ");
+            scanf("%d",&qiz);
+        }
+        return marks;
+}}
